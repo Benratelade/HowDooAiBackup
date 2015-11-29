3814dc5e08d83c8f514f5eb1acef076341a7614d
Ben Ratelade iMac <benjamin@empireone.com.au>
2015-11-30 07:54:44 +1100

    Modified scheduler to run only once a day, at midnight.

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,13 +2,13 @@ require 'rufus-scheduler'
 
 @queue_build_scheduler = Rufus::Scheduler.new
 
-@queue_build_scheduler.cron('*/1 * * * *') do 
+@queue_build_scheduler.cron('0 0 * * *') do 
 	build_backup_queue
 end
 
 @process_queue_scheduler = Rufus::Scheduler.new
 
-@process_queue_scheduler.cron('*/2 * * * *') do
+@process_queue_scheduler.cron('10 0 * * *') do
 	process_backup_queue
 end
 
