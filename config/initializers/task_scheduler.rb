f60a92e05fbe70479cd9fce24ab5a143b21f5441
Ben Ratelade iMac <ratelade.benjamin@gmail.com>
2016-02-15 23:59:36 +1100

    using Delayed Jobs to build queue

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,43 +1,44 @@
-require 'rufus-scheduler'
+# require 'rufus-scheduler'
 
-@queue_build_scheduler = Rufus::Scheduler.new
+# @queue_build_scheduler = Rufus::Scheduler.new
 
-@queue_build_scheduler.cron('/2 * * * *') do 
-	build_backup_queue
-end
+# @queue_build_scheduler.cron('* * * * *') do 
+# 	build_backup_queue
+# end
 
-@process_queue_scheduler = Rufus::Scheduler.new
+# # @process_queue_scheduler = Rufus::Scheduler.new
 
-@process_queue_scheduler.cron('/3 * * * *') do
-	process_backup_queue
-end
+# # @process_queue_scheduler.cron('* * * * *') do
+# # 	process_backup_queue
+# # end
 
-def build_backup_queue
-	puts "Building backups queue"
-	backups = Backup.where(next_backup_date: Date.today, queued: false)
-	@backups_queue ||= []
-	backups.each do |backup|
-		backup.queued == true
-		@backups_queue << backup
-	end
-	Backup.transaction do 
-		@backups_queue.each do |backup|
-			backup.save
-		end
-	end
-end
+# def build_backup_queue
+# 	puts "Building backups queue"
+# 	backups = Backup.where(next_backup_date: Date.today, queued: false)
+# 	@backups_queue ||= []
+# 	backups.each do |backup|
+# 		backup.delay(queue: 'backups').execute_transfer
+# 		@backups_queue << backup
+# 	end
+# 	Backup.transaction do 
+# 		@backups_queue.each do |backup|
+# 			backup.save
+# 		end
+# 	end
+# end
+# handle_asynchronously :build_backup_queue
 
-def process_backup_queue
-	if @backups_queue
-		puts "About to process the following backups: #{@backups_queue}"
-		puts "before starting queue"
-		@backups_queue.each do |backup|
-			puts backup
-			puts "starting backup process from process_backup_queue"
-			backup.execute_transfer
-			@backups_queue.delete(backup)
-			backup.queued = false
-			backup.save
-		end
-	end
-end
+# # def process_backup_queue
+# # 	if @backups_queue
+# # 		puts "About to process the following backups: #{@backups_queue}"
+# # 		puts "before starting queue"
+# # 		@backups_queue.each do |backup|
+# # 			puts backup
+# # 			puts "starting backup process from process_backup_queue"
+# # 			backup.execute_transfer
+# # 			@backups_queue.delete(backup)
+# # 			backup.queued = false
+# # 			backup.save
+# # 		end
+# # 	end
+# # end
